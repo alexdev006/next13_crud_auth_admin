dac15443b762d581a5b10a4e4dc139f4832e188f
Alex <alexdev0605@gmail.com>
2023-05-31 22:08:20 +0200

    first post

---
+++ src/app/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className="">2023 alex. All rights reserved</div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Footer;
